c7137624a7fb042a021b2ca8eddd0d5a7bf60c14
eraugust <eraugust@student.42sp.org.br>
2022-02-19 03:29:55 +0100

    finished - basecamp42 - 2020

--- C/C05/ex01/ft_recursive_factorial.c
+++ C/C05/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 21:40:29 by eraugust          #+#    #+#             */
-/*   Updated: 2022/02/18 22:52:53 by eraugust         ###   ########.fr       */
+/*   Updated: 2022/02/19 01:09:40 by eraugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	if (nb < 0)
+	if (nb > 0)
 		return (nb * ft_recursive_factorial(nb - 1));
 	else
 		return (1);
--- C/C05/ex05/ft_sqrt.c
+++ C/C05/ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: eraugust <eraugust@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 21:22:21 by eraugust          #+#    #+#             */
-/*   Updated: 2022/02/18 22:16:00 by eraugust         ###   ########.fr       */
+/*   Updated: 2022/02/19 03:27:32 by eraugust         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,3 +30,46 @@ int	ft_sqrt(int nb)
 	}
 	return (0);
 }
+
+
+
+int	ft_bisec(int nb);
+
+int	ft_sqrt(int nb)
+{	
+	if (nb < 1)
+		return (0);
+	if (nb == 1)
+		return (1);
+	return (ft_bisec(nb));
+}
+
+/// - - ok s/ time out
+// int	ft_bisec(int nb)
+// {
+// 	int	end;
+// 	int	beg;
+// 	int	g;
+
+// 	end = nb;
+// 	beg = 0;
+// 	g = (end + beg) / 2;
+// 	while (g * g != nb)
+// 	{
+// 		if (g * g > nb)
+// 		{
+// 			end = g;
+// 			beg = beg;
+// 			g = (end + beg) / 2;
+// 		}
+// 		if (g * g < nb)
+// 		{
+// 			end = end;
+// 			beg = g;
+// 			g = (end + beg) / 2;
+// 		}
+// 		if (((end - beg) == 1) && ((g == end) || (g == beg)))
+// 			return (0);
+// 	}
+// 	return (g);
+// }
